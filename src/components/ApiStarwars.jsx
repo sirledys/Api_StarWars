1062b3972361d8cfea0d18d20ca9d9c6efa9a92e
sirledys <sirledys.rangel@gmail.com>
2023-05-25 11:24:33 -0500

    Commit inicial botones y título

---
+++ src/components/ApiStarwars.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ApiStarwars = () => {
+  return (
+    <div>
+        <h1>Petición al Api de Star Wars</h1>
+      <button >Atrás</button>
+      <button >Siguiente</button>
+    </div>
+  )
+}
+
+export default ApiStarwars
