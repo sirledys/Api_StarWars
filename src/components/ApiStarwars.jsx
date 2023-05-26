8420081d8e243313454aa44f5c64a203b2d56b6c
sirledys <sirledys.rangel@gmail.com>
2023-05-26 00:13:06 -0500

    Ultimas correcciones

--- src/components/ApiStarwars.jsx
+++ src/components/ApiStarwars.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 const ApiStarwars = () => {
 
 const [personajes, setPersonajes] = React.useState([]);
@@ -9,6 +8,7 @@ const TraerPersonajes = async (page) => {
     try {
       const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
       const { results } = await res.json();
+
       const personajesConImagen = results.map((personaje) => ({
         ...personaje,
         image: `https://starwars-visualguide.com/assets/img/characters/${extractIdFromUrl(personaje.url)}.jpg`,
@@ -37,27 +37,28 @@ const TraerPersonajes = async (page) => {
     return urlParts[urlParts.length - 2];
   };
   return (
-    <div>
+      <div>
         <h1>Petición al Api de Star Wars</h1>
-      <button onClick={atras}>Atrás</button>
-      <button onClick={siguiente}>Siguiente</button>
+        <img src='https://i.pinimg.com/originals/b6/af/5a/b6af5aeff0ee43a4253fc70c167bb6db.png' class='img1'></img>
+        <button onClick={atras}>Atrás</button>
+        <button onClick={siguiente}>Siguiente</button>
 
-      {
-        personajes.map(({name, eye_color, gender, mass, image}) => (
+        {
+          personajes.map(({name, eye_color, gender, mass, image}) => (
 
-          <div key= {name}>
-              <h3>Nombre: {name}</h3>
-              <h4>Color de ojos: {eye_color}</h4>
-              <h4>Género: {gender}</h4>
-              <h4>Peso en Kg: {mass}</h4>
-             <img src={image} alt={name} />
-            </div>
-        )
-        )
-      }
+            <div key= {name}>
+                <h3>Nombre: {name}</h3>
+                <h4>Color de ojos: {eye_color}</h4>
+                <h4>Género: {gender}</h4>
+                <h4>Peso en Kg: {mass}</h4>
+               <img src={image} alt={name} />
+              </div>
+          )
+          )
+        }
 
-    </div>
-  )
+      </div>
+  );
 }
 
 export default ApiStarwars
